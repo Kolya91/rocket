58f865f08f680abfbe0bf567bdccd302ece6e71c
Kolya Nechay <kolyanechay1991@gmail.com>
2020-04-23 18:01:42 +0300

    Update tslint rules, add alias config

---
+++ src/app/+todos/todos-container.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todos-container',
+  templateUrl: './todos-container.component.html'
+})
+export class TodosContainerComponent implements OnInit {
+
+  public ngOnInit(): void {}
+}
