b80f4a52ee457a3383805eaca5379a2365f7c969
Kolya Nechay <kolyanechay1991@gmail.com>
2020-04-24 18:50:22 +0300

    Add jest

--- src/app/__tests__/app.component.spec.ts
+++ src/app/__tests__/app.component.spec.ts
@@ -1,7 +1,8 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './../app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -12,6 +13,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
